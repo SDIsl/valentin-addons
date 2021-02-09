29b6adc636446764965488f8f41bf7efa44233b5
valentincastravete <58530982+valentincastravete@users.noreply.github.com>
2021-02-09 11:48:06 +0100

    Update product_template.py

--- sysadmin_base/models/product_template.py
+++ sysadmin_base/models/product_template.py
@@ -7,15 +7,30 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    internal_equipment = fields.Boolean(string='Internal equipment')
+    internal_equipment = fields.Boolean(
+        string='Internal equipment'
+    )
 
-    information = fields.Text(string='Information')
+    information = fields.Text(
+        string='Information'
+    )
 
     hardware_properties = fields.Boolean(
-        readonly=True, store=True, related='categ_id.show_hardware_properties')
+        readonly=True, store=True, related='categ_id.show_hardware_properties'
+    )
 
-    cpu = fields.Char(string='CPU')
-    ram = fields.Char(string='RAM')
-    data_storage = fields.Char(string='Data storage')
-    ip = fields.Char(string='IP')
-    os_version = fields.Char(string='OS Version')
+    cpu = fields.Char(
+        string='CPU'
+    )
+    ram = fields.Char(
+        string='RAM'
+    )
+    data_storage = fields.Char(
+        string='Data storage'
+    )
+    ip = fields.Char(
+        string='IP'
+    )
+    os_version = fields.Char(
+        string='OS Version'
+    )
