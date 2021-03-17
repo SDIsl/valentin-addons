a6b963718aff402c69ee988a8e05d6ed3710fd1d
Valentin Castravete <valentincastravete@gmail.com>
2021-03-17 16:59:33 +0100

    Quitar módulo hr_timesheet_calendar

--- sysadmin_base/models/__init__.py
+++
@@ -1,9 +0,0 @@
-###############################################################################
-# For copyright and license notices, see __manifest__.py file in root directory
-###############################################################################
-from . import hr_employee
-from . import product_category
-from . import product_template
-from . import workspace_item
-from . import workspace_item_subsidy
-from . import workspace_workspace
